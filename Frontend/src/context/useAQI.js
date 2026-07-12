875b5efda88ab82caa0cce25816c0087467ae30c
ladybugnita <nitadangol0@gmail.com>
2026-07-12 20:09:45 +0545

    Remove Frontend, keeping only Backend and .github

--- Frontend/src/context/useAQI.js
+++
@@ -1,9 +0,0 @@
-import { createContext, useContext } from "react"
-
-export const AQIContext = createContext(null)
-
-export function useAQI() {
-  const ctx = useContext(AQIContext)
-  if (!ctx) throw new Error("useAQI must be used within AQIProvider")
-  return ctx
-}
--- Frontend/src/data/config.js
+++
@@ -1,8 +0,0 @@
-
-export const REFRESH_MS = 15000
-
-export const CITIES_REFRESH_MS = 60000
-
-export const TREND_REFRESH_MS = 300000
-
-export const TICK_MS = 1000
